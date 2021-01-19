01b9b1f85614b3bdb79eff8f73e5b31fcc7cd96b
Darek Kilian <emis@MacBook-Pro.local>
2021-01-19 21:16:19 +0100

    new contacts provider

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,9 +2,17 @@ import React, { useState } from "react";
 import Login from "./Login";
 import useLocalStorage from "../hooks/useLocalStorage";
 import Dashboard from "../components/Dashboard";
+import { ContactsProvider } from "../context/ContactsProvider";
+
 function App() {
   const [id, setId] = useLocalStorage("id");
-  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  );
+
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
 export default App;
